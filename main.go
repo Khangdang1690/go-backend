87d96b0c0d1ec7da857d1d4f275ae2f2ae4bb3a9
Khang Dang <khangdang0199@gmail.com>
2025-06-30 20:42:02 -0700

    2

--- main.go
+++ main.go
@@ -1,39 +1,15 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"encoding/json"
+	"github.com/gin-gonic/gin"
 )
 func main() {
-	http.HandleFunc("/demo", demoHandler)
-	log.Println("Server is starting...")
-	er := http.ListenAndServe(":8080", nil)
-	if er != nil {
-		log.Fatal("Error starting server:", er)
-	}
-}
+	r := gin.Default()
+	r.GET("/demo", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
 
-func demoHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%+v", r)
-
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	response := map[string]string{
-		"message": "Hello, World!",
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-
-	// data, err := json.Marshal(response)
-	// if err != nil {
-	// 	http.Error(w, "Internal server error", http.StatusInternalServerError)
-	// 	return
-	// }
-
-	// w.Write(data)
-	json.NewEncoder(w).Encode(response)
+	r.Run(":8080") // Start the server on port 8080
 }
